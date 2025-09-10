0815c69b2b17c64ca1f7ab064578d39a35d82e41
hanjiewaiduanle <132766656+hanjiewaiduanle@users.noreply.github.com>
2025-09-10 16:28:56 +0800

    Add files via upload
    登录页面设计

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,40 +1,55 @@
-package com.example.hi
+package com.example.logininterface
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.hi.ui.theme.HITheme
+import com.example.logininterface.ui.theme.LoginInterfaceTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HITheme {
+            LoginInterfaceTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy New Day LZD!",
-                        from = "From LZD",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    LogInInterfaceLayout()
                 }
             }
         }
@@ -42,31 +57,85 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+fun LogInInterfaceLayout() {
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
+            text = stringResource(R.string.log_in),
+            fontSize = 40.sp,
             modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .padding(bottom = 20.dp, top = 60.dp)
+                .align(alignment = Alignment.CenterHorizontally)
         )
+
+        Text(
+            text = stringResource(R.string.account),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+
+        EditAccountField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+
+        Text(
+            text = stringResource(R.string.password),
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .align(alignment = Alignment.Start)
+        )
+
+        EditPasswordField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(stringResource(R.string.log_in))
+
+        }
+
     }
+}
+
+
+@Composable
+fun EditAccountField(modifier: Modifier = Modifier) {
+    var accountInput by remember { mutableStateOf("") }
+    TextField(
+        value = accountInput,
+        onValueChange = {accountInput = it},
+        modifier = modifier,
+        label = { Text(stringResource(R.string.account)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+    )
+}
+
+@Composable
+fun EditPasswordField(modifier: Modifier = Modifier) {
+    var accountInput by remember { mutableStateOf("") }
+    TextField(
+        value = accountInput,
+        onValueChange = {accountInput = it},
+        modifier = modifier,
+        label = { Text(stringResource(R.string.password)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun BirthdayCardPreview() {
-    HITheme {
-        GreetingText("Happy New Day LZD!", "From LZD")
+fun InterfaceLayoutPreview() {
+    LoginInterfaceTheme {
+        LogInInterfaceLayout()
     }
 }
